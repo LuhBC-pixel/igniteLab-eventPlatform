c41cf537450d9bc23c4b85662760b8223a6f8931
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-06-22 21:55:38 -0300

    inicio da terceira aula

---
+++ README.md
@@ -0,0 +1,2 @@
+Parei no 41:40
+https://evento.rocketseat.com.br/ignite-lab/episodios/react/aula-3/edicao/1
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Home } from './pages/Home';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './lib/apollo';
+import { Router } from './Router';
 
 function App() {
   return (
-    <div>
-     <Home />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
